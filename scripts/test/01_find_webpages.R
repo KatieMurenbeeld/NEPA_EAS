c4263271e7442ddcc409d7524968125bcfe36637
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-02-02 22:00:44 -0700

    more rvest testing

--- scripts/test/01_find_webpages.R
+++ scripts/test/01_find_webpages.R
@@ -77,3 +77,25 @@ projs_1000 <- data.frame(projs$PROJECT.NUMBER[1:1000], projs$PROJECT.NAME[1:1000
                         overview_h1_out$overview_h1)
 
 #projs_2009_all <- 
+
+# -----------------------
+# New rvest testing see https://www.zenrows.com/blog/web-scraping-r#retrieve-html 
+# retrieving the target web page 
+document <- read_html("https://www.fs.usda.gov/project/?project=57069&exp=overview")
+
+# selecting the list of product HTML elements 
+html_products <- document %>% html_elements("a.item-link")
+
+document %>% html_elements("a") %>% html_attr("href")
+
+html_divs <- document %>% html_elements("div") 
+
+document %>% html_elements("iframe") 
+
+document %>% html_elements("html") %>% html_attrs("href")
+
+document %>% html_elements("div.item-name")
+
+document %>% html_nodes(xpath = '//*[@id="contextmenutarget16"]/div/div[2]/div/div[1]/div[1]/div')
+
+document %>% html_nodes(xpath = '/html/body/div[1]/div[5]/span/div/main/div/div/div[2]/div/div[2]/div/div/div[1]/div/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div[1]/div/a')
