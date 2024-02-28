70a8b246e9dbdaefe0d43ceb73e353a5fd37b6a6
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-02-28 12:40:24 -0700

    Updated comments

--- scripts/test/03_EA_data_exploration.R
+++ scripts/test/03_EA_data_exploration.R
@@ -11,35 +11,39 @@ sub_df <- facts_df %>%
 
 ## Create some plots or show summary stats
 
+### Filter out forests in Region 10
 sub_df <- sub_df %>%
   filter(FOREST_ID < 1000)
 
 #sub_df$DECISION.SIGNED <- as_datetime(sub_df$DECISION.SIGNED)
 
+# Histogram of elapsed days
 ggplot(sub_df, aes(x=ELAPSED.DAYS)) + 
   geom_histogram()
-
+# Histogram of elapsed days by Region
 ggplot(sub_df, aes(x=ELAPSED.DAYS, color=LMU...REGION)) + 
   geom_histogram(fill="white") + 
   facet_wrap(~LMU...REGION)
 
+# Histogram of number of activities
 ggplot(sub_df, aes(x=NBR_ACTIVITIES)) + 
   geom_histogram()
-
+# Histogram of number of activities by Region
 ggplot(sub_df, aes(x=NBR_ACTIVITIES, color=LMU...REGION)) + 
   geom_histogram(fill="white") + 
   facet_wrap(~LMU...REGION)
 
+# Bar plot of the number of EAs completed per Region
 reg_counts <- sub_df %>% count(LMU...REGION)
 
 ggplot(reg_counts, aes(x=LMU...REGION, y=n, color=LMU...REGION)) + 
   geom_bar(fill="white", stat = "identity") 
-
+# Line plot of the number of EAs completed per year
 year_counts <- sub_df %>% count(calendarYearSigned)
 
 ggplot(year_counts, aes(x=calendarYearSigned, y=n)) + 
   geom_line()
-
+# Line plots of the number of EAs completed per Region by year
 reg_year_counts <- sub_df %>%
   group_by(LMU...REGION) %>%
   count(calendarYearSigned)
@@ -48,6 +52,7 @@ ggplot(reg_year_counts, aes(x=calendarYearSigned, y=n, color=LMU...REGION)) +
   geom_line() + 
   facet_wrap(~LMU...REGION)
 
+# Bar plot of the number of EAs signed per month
 months_signed <- ymd(sub_df$DECISION.SIGNED)
 
 months_signed_v <- month(months_signed)
@@ -64,6 +69,7 @@ month_signed_reg_counts <- sub_df %>%
 ggplot(month_signed_counts, aes(x=month_signed, y=n)) + 
   geom_bar(stat = "identity") 
 
+# Bar plot of the number of EAs signed per month per Region
 ggplot(month_signed_reg_counts, aes(x=month_signed, y=n, color=LMU...REGION)) +
   geom_bar(stat = "identity") +
   facet_wrap(~LMU...REGION) +
