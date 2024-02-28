cc59fd646209dce4da7fb76798e984d610a5cdd5
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-02-28 14:28:36 -0700

    Need to figure out how to delete the .zip file once it is unzipped. Or do I want to do that? maybe just skip

--- scripts/test/04_EA_documents_metadata.R
+++ scripts/test/04_EA_documents_metadata.R
@@ -2,19 +2,27 @@ library(tidyverse)
 library(stringr)
 library(ggplot2)
 
-
+# set output directory
+outdir <- "/Users/katiemurenbeeld/Analysis/NEPA_EAs/data/original/NEPA_DOCS"
 # for each project
 ## unzip documents
 ## count total documents and documents by type (based on folder names)
 
-folder_list <- list.dirs( path = "/Users/katiemurenbeeld/Analysis/NEPA_EAs/data/original/NEPA_DOCS", full.names = TRUE, recursive = FALSE)
+#folder_list <- list.dirs( path = "/Users/katiemurenbeeld/Analysis/NEPA_EAs/data/original/NEPA_DOCS", full.names = TRUE, recursive = FALSE)
+zip_list <- list.files(path = "/Users/katiemurenbeeld/Analysis/NEPA_EAs/data/original/NEPA_DOCS", pattern=".zip")
+
+#unzip(paste0(outdir, "/", zip_list[1]), exdir = outdir)
 
 meta_data <- data.frame()
 
-for (i in folder_list[1:3]) {
-  #print(paste0(i, "/"))
-  tmp <- list.files(path = paste0(i, "/"), pattern = ".pdf", recursive = TRUE)
-  project_num <- str_extract(i, "(?<=\\().+?(?=\\))") 
+for (i in zip_list[1:3]) {
+  print(i)
+  unzip(paste0(outdir, "/", i), exdir = outdir)
+}
+folder_list <- list.dirs( path = "/Users/katiemurenbeeld/Analysis/NEPA_EAs/data/original/NEPA_DOCS", full.names = TRUE, recursive = FALSE)
+for (n in folder_list){
+  tmp <- list.files(path = paste0(n, "/"), pattern = ".pdf", recursive = TRUE)
+  project_num <- str_extract(n, "(?<=\\().+?(?=\\))") 
   prescoping <- length(str_subset(tmp, "/Pre-Scoping/"))
   scoping <- length(str_subset(tmp, "/Scoping/"))
   supporting <- length(str_subset(tmp, "Supporting"))
@@ -25,7 +33,9 @@ for (i in folder_list[1:3]) {
   output <- c(project_num, prescoping, scoping, supporting, analysis, assessment, decision, appeals)
   meta_data <- rbind(meta_data, output)
 }
+
 colnames(meta_data) <- c("Project_Number", "Prescoping", "Scoping", "Supporting", "Analysis", "Assessment", "Decision", "Appeals")
+
 
 # rename all documents with project number and document type in the file name
 
