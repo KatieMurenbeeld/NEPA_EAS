65c5c99bf34df8320ef2c3ff762841e7bcdb2931
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-03-19 12:08:29 -0600

    added comments load all regions

--- scripts/test/00_PALS-FACTS.R
+++ scripts/test/00_PALS-FACTS.R
@@ -1,10 +1,16 @@
 library(tidyverse)
 library(sf)
 
+# Set timeout
 options(timeout = 3600)
 
+#---Load the data----------
+
+# Load the PALS data (from Cory)
 pals_ongoing <- readRDS("~/Analysis/NEPA_EAs/scripts/pals_ongoing_projects_thru_3-2023.rds")
 
+
+# Download the FS common attribute databases
 reg_lst <- c("01", "02", "03", "04", "05", "06", "08", "09")
 
 zip_url <- paste0("https://data.fs.usda.gov/geodata/edw/edw_resources/fc/Actv_CommonAttribute_PL_Region")
@@ -28,6 +34,9 @@ for (i in fnames) {
   unzip(i, exdir = here::here("data/original/"))
 }
 
+#---Subset the CommonAttribute geodatabases------
+
+# could make a function and loop for this
 reg1 <- st_read(here::here("data/original/Actv_CommonAttribute_PL_Region01.gdb"),
                 query = "SELECT * from Actv_CommonAttribute_PL WHERE FISCAL_YEAR_PLANNED > 2005")
 reg2 <- st_read(here::here("data/original/Actv_CommonAttribute_PL_Region02.gdb"),
@@ -36,4 +45,13 @@ reg3 <- st_read(here::here("data/original/Actv_CommonAttribute_PL_Region03.gdb")
                 query = "SELECT * from Actv_CommonAttribute_PL WHERE FISCAL_YEAR_PLANNED > 2005")
 reg4 <- st_read(here::here("data/original/Actv_CommonAttribute_PL_Region04.gdb"), 
                 query = "SELECT * from Actv_CommonAttribute_PL WHERE FISCAL_YEAR_PLANNED > 2005")
+reg5 <- st_read(here::here("data/original/Actv_CommonAttribute_PL_Region05.gdb"), 
+                query = "SELECT * from Actv_CommonAttribute_PL WHERE FISCAL_YEAR_PLANNED > 2005")
+reg6 <- st_read(here::here("data/original/Actv_CommonAttribute_PL_Region06.gdb"), 
+                query = "SELECT * from Actv_CommonAttribute_PL WHERE FISCAL_YEAR_PLANNED > 2005")
+reg8 <- st_read(here::here("data/original/Actv_CommonAttribute_PL_Region08.gdb"), 
+                query = "SELECT * from Actv_CommonAttribute_PL WHERE FISCAL_YEAR_PLANNED > 2005")
+reg9 <- st_read(here::here("data/original/Actv_CommonAttribute_PL_Region09.gdb"), 
+                query = "SELECT * from Actv_CommonAttribute_PL WHERE FISCAL_YEAR_PLANNED > 2005")
+
 
