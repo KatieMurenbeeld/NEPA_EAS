9c3085f053615a99a66d29ed0db25e228c8a454d
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-04-02 15:26:25 -0600

    updated to work with EIS projects

--- scripts/test/03_download_documents.R
+++ scripts/test/03_download_documents.R
@@ -13,11 +13,18 @@ pinyon_urls$project_number <- as.character(pinyon_urls$project_number)
 pals$PROJECT.NUMBER <- as.character(pals$PROJECT.NUMBER)
 pinyon_pals <- left_join(pinyon_urls, pals, by = c("project_number" = "PROJECT.NUMBER"))
 pinyon_eas <- pinyon_pals %>%
-  filter(DECISION.TYPE == "DN")
+  filter(DECISION.TYPE == "DN") 
 min(pinyon_eas$calendarYearSigned)
+
+pinyon_eis <- pinyon_pals %>%
+  filter(DECISION.TYPE == "ROD")
+min(pinyon_eis$calendarYearSigned)
 
 pinyon_eas_urls <- pinyon_eas %>%
   filter(grepl("https://usfs-public.app.box.com/v", pinyon_eas[,1]) == TRUE)
+
+pinyon_eis_urls <- pinyon_eis %>%
+  filter(grepl("https://usfs-public.app.box.com/v", pinyon_eis[,1]) == TRUE)
 
 ## Use chromote as a headless browser with R to navigate to a url and click on the download button
 ### will need to find the x and y position of the download button
@@ -37,14 +44,16 @@ pinyon_eas_urls <- pinyon_eas %>%
 
 # Check number of zip files downloaded
 length(list.files(here::here("data/original/NEPA_DOCS/")))
+length(list.files(here::here("data/original/NEPA_DOCS/EISs")))
 
 b <- ChromoteSession$new(wait_ = TRUE)
 x <- 1650 # 50 pixels less than the width of the browser set in the for loop
 y <- 100
 
-for (i in pinyon_eas_urls[1312:1803,1]) { # update index based on number of zip files downloaded.
+for (i in pinyon_eis_urls[,1]) { # update index based on number of zip files downloaded.
   tmp <- b$new_session(width = 1700, height = 1800, wait_ = TRUE)
-  tmp$Browser$setDownloadBehavior("allow", downloadPath = "/Users/katiemurenbeeld/Analysis/NEPA_EAs/data/original/NEPA_DOCS/")
+  # update download path for EIS documents
+  tmp$Browser$setDownloadBehavior("allow", downloadPath = "/Users/katiemurenbeeld/Analysis/NEPA_EAs/data/original/NEPA_DOCS/EISs")
   tmp$Page$navigate(i)
   Sys.sleep(runif(1, 3, 4))
   tmp$Input$dispatchMouseEvent(type = "mousePressed", x = x, y = y, button="left", clickCount=1)
